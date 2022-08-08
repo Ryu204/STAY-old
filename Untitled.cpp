c7c0cf58bf0489efe18e5d05e8e41a6f0fab7bac
Nguyen Anh Bao <baotrass@gmail.com>
2022-08-09 00:11:01 +0700

    Add project files.

---
+++ Untitled.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <lua.hpp>
+
+int main()
+{
+	sf::RenderWindow win(sf::VideoMode(100, 100), "Window");
+	while (win.isOpen())
+	{
+		sf::Event e;
+		while (win.pollEvent(e))
+		{ 
+			if (e.type == sf::Event::Closed)
+				win.close();
+		}
+
+		win.clear(sf::Color::Blue);
+		win.display();
+	}
+}
